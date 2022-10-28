c3c3a84b6d1b3f8b5e6092c5880e2ce75313a06a
Trishiraj <gamerprince.exp@gmail.com>
2022-10-28 21:50:22 +0000

    Bump to vT5.1
    and minor changes while at it
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Change-Id: I3f9920205e17713ae0a0366bfeb2dfc327db6352

--- config/branding.mk
+++ config/branding.mk
@@ -1,9 +1,9 @@
 # Set all versions
 RAVEN_LAIR := Unofficial
-CORVUS_VERSION_BASE := T5.0
-CORVUS_CODENAME := El-Dorado
+CORVUS_VERSION_BASE := T5.1
+CORVUS_CODENAME := DejaVu
 
-ifneq ($(filter Trishiraj victor10520 KSSRAO ManavjitSingh MAdMiZ Fosslover Meghthedev PAiN Roxor-007 H3M3L AnhTran Sukeerat EinarG Ankan005 Ajit YadavMohit19 Ritzzz TheSanty CoolDude6942,$(CORVUS_MAINTAINER)),)
+ifneq ($(filter Trishiraj victor10520 KSSRAO ManavjitSingh MAdMiZ Fosslover Meghthedev PAiN KaderBava Roxor-007 H3M3L AnhTran Sukeerat EinarG Ankan005 Ajit YadavMohit19 Ritzzz,$(CORVUS_MAINTAINER)),)
 RAVEN_LAIR := Official
 endif
 
@@ -16,5 +16,5 @@ ROM_FINGERPRINT := Corvus/$(RAVEN_LAIR)/$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell 
 ifeq ($(USE_GAPPS), true)
     CORVUS_VERSION := Corvus_v$(CORVUS_VERSION_BASE)-$(CORVUS_CODENAME)-$(CORVUS_BUILD)-Gapps-$(RAVEN_LAIR)-$(BUILD_TIME)
 else
-    CORVUS_VERSION := Corvus_v$(CORVUS_VERSION_BASE)-$(CORVUS_CODENAME)-$(CORVUS_BUILD)-$(RAVEN_LAIR)-$(BUILD_TIME)
+    CORVUS_VERSION := Corvus_v$(CORVUS_VERSION_BASE)-$(CORVUS_CODENAME)-$(CORVUS_BUILD)-$(RAVEN_LAIR)-BETA-$(BUILD_TIME)
 endif
